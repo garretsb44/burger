e8784b1d5f0eac03c26a5987d1e3116df6f2f05a
Garret Bright <garret@drinknewtopia.com>
2020-10-16 09:56:12 -0700

    finally fixed toggle buttons

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -3,13 +3,13 @@ $(() => {
     const id = $(this).data("id");
     const newDevour = $(this).data("newdevour");
 
-    const newDevouredState = { value: newDevour };
+    const newDevourState = { value: newDevour };
 
     // Send the PUT request.
     $.ajax(`/api/burgers/${id}/devoured`, {
       type: "PUT",
       // Convert object to JSON
-      data: JSON.stringify(newDevouredState),
+      data: JSON.stringify(newDevourState),
       // Tell the server that this request contains JSON
       contentType: "application/json; charset=UTF-8",
     }).then(() => {
@@ -17,24 +17,23 @@ $(() => {
       location.reload();
     });
   });
-  
-    $(".create-form").on("submit", (event) => {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      const newBurger = {
-        burger_name: $("#ca").val().trim(),
-        devoured: $("[name=devoured]:checked").val().trim(),
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger,
-      }).then(() => {
-        // Reload the page to get the updated list
-        location.reload();
-      });
+
+  $(".create-form").on("submit", (event) => {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    const newBurger = {
+      burger_name: $("#ca").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim(),
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(() => {
+      // Reload the page to get the updated list
+      location.reload();
     });
   });
-  
+});
